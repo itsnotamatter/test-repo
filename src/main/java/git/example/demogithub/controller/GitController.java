5843f7cd15c1f282120c1391c85d960d00d3fbbc
itsnotamatter <55035425+itsnotamatter@users.noreply.github.com>
2022-04-02 14:00:21 +0300

    first commit

---
+++ src/main/java/git/example/demogithub/controller/GitController.java
@@ -0,0 +1,22 @@
+package git.example.demogithub.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class GitController {
+
+    @GetMapping("/user")
+    public String user() {
+        return "User";
+    }
+
+    @GetMapping("/admin")
+    public String admin() {
+        return "Admin";
+    }
+
+
+}
