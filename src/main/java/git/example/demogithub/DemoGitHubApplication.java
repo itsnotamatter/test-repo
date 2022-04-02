5f3b22ac5b85b57e208517877c33445d52e5e975
itsnotamatter <jsscob@gmail.com>
2022-04-02 14:54:54 +0300

    demo comment n2

--- src/main/java/git/example/demogithub/DemoGitHubApplication.java
+++ src/main/java/git/example/demogithub/DemoGitHubApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoGitHubApplication {
+public class DemoGitHubApplication {//test
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoGitHubApplication.class, args);
